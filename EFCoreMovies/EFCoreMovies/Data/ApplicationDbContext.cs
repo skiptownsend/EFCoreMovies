89882d8df3cfbbb1534b0126ab241a0e64a7cd62
Skip Townsend <skiptownsend@outlook.com>
2023-02-03 19:43:06 -0700

    Setup project, create Genre Entity

---
+++ EFCoreMovies/EFCoreMovies/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreMovies.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Genre> Genres { get; set; }
+    }
+}
